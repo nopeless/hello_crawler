1c9e7a9829218bfc7bd5542056ac7ca50412edc5
nopeless <38830903+nopeless@users.noreply.github.com>
2024-11-23 02:02:00 -0600

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HelloCrawler.Mixfile do
     [
       app: :hello_crawler,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -22,8 +22,8 @@ defmodule HelloCrawler.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
-      {:floki, "~> 0.18.0"}
+      {:httpoison, "~> 2.2.1"},
+      {:floki, "~> 0.36.0"}
     ]
   end
 end
